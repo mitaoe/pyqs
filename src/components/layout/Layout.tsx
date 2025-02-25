915e3ce36946813896aba29d916f61cb25070f1b
aditya <adityakotkar47@gmail.com>
2025-02-25 11:40:10 +0530

    style: add browse padding

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -11,7 +11,7 @@ export default function Layout({ children }: LayoutProps) {
     <div className="flex h-screen select-none flex-col bg-primary text-content">
       <Header />
       <main className="flex-1 overflow-hidden">
-        <div className="h-full overflow-y-auto px-4">
+        <div className="h-full overflow-y-auto px-4 py-8">
           <div className="container mx-auto">
             {children}
           </div>
